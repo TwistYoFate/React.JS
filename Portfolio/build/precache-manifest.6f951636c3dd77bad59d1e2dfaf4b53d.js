5f6b828d71404d40982eb6d277a92358784827e5
Deepanshu Yadav <daybond007@gmail.com>
2019-05-06 14:26:12 +0530

    upadate

---
+++ Portfolio/build/precache-manifest.6f951636c3dd77bad59d1e2dfaf4b53d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "dac0175384f89899c397116440789449",
+    "url": "/TwistYoFate/React.JS/static/media/pic.dac01753.jpg"
+  },
+  {
+    "revision": "4db3a08bc86fc57fdfe6ad43fd558473",
+    "url": "/TwistYoFate/React.JS/static/media/ml.4db3a08b.png"
+  },
+  {
+    "revision": "7e03175d4390d6b0435a3d514fd0edb8",
+    "url": "/TwistYoFate/React.JS/static/media/gh.7e03175d.png"
+  },
+  {
+    "revision": "8134798c9daea34ba0c2222ca3124bde",
+    "url": "/TwistYoFate/React.JS/static/media/ct.8134798c.png"
+  },
+  {
+    "revision": "021911c0a2674c8cab11",
+    "url": "/TwistYoFate/React.JS/static/js/runtime~main.021911c0.js"
+  },
+  {
+    "revision": "0bb0de309f5dd1e0d433",
+    "url": "/TwistYoFate/React.JS/static/js/main.0bb0de30.chunk.js"
+  },
+  {
+    "revision": "0549845eeefae04a5bdd",
+    "url": "/TwistYoFate/React.JS/static/js/2.0549845e.chunk.js"
+  },
+  {
+    "revision": "0bb0de309f5dd1e0d433",
+    "url": "/TwistYoFate/React.JS/static/css/main.8124a59c.chunk.css"
+  },
+  {
+    "revision": "0549845eeefae04a5bdd",
+    "url": "/TwistYoFate/React.JS/static/css/2.17eedf3e.chunk.css"
+  },
+  {
+    "revision": "9bb022e6d0657934f44afe6e7062de65",
+    "url": "/TwistYoFate/React.JS/index.html"
+  }
+];
